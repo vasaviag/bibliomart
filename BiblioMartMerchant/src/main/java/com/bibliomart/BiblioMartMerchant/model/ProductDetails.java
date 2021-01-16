19d17e5be0c0d68d6fee4b29dc73380acdc756db
Kannuru Dinesh <kannurudinesh@Kannurus-MacBook-Pro.local>
2021-01-16 16:18:00 +0530

    Updated merchant microservice

--- BiblioMartMerchant/src/main/java/com/bibliomart/BiblioMartMerchant/model/ProductDetails.java
+++ BiblioMartMerchant/src/main/java/com/bibliomart/BiblioMartMerchant/model/ProductDetails.java
@@ -5,6 +5,52 @@ public class ProductDetails {
     private String categoryName;
     private int cost;
     private int quantity;
+    private String productDescription;
+    private String edition;
+    private String author;
+    private String language;
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    private String publisher;
 
     public String getProductName() {
         return productName;
